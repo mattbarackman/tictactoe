11191ff023b6f9cb12f463c3fe36195eedf994be
mattbarackman <matthew.barackman@gmail.com>
2013-06-14 16:31:54 -0700

    get persistent form data when form invalid

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,12 +7,22 @@ end
 
 get '/sessions/new' do
   # render sign-in page
-  @errors = session.delete(:errors) if session[:errors] 
+  p @errors = errors 
   erb :sign_in
 end
 
 post '/sessions' do
-  
+  user = User.find_by_email(params[:email])
+  if user.nil?
+    session[:errors] = {username: "not found"}
+    redirect '/sessions/new'
+  elsif user.authenticate(params[:password]) == false
+    session[:errors] = {password: "incorrect"}
+    redirect '/sessions/new'    
+  else
+    session[:user_id] = user.id
+    redirect '/'
+  end
 end
 
 delete '/sessions/:id' do
@@ -23,12 +33,12 @@ end
 
 get '/users/new' do
   # render sign-up page
-  @errors = session.delete(:errors) if session[:errors] 
+  @form_data = session.delete(:form_data) if session[:form_data]
+  @errors = errors 
   erb :sign_up
 end
 
 post '/users' do
-  p params[:user]
   user = User.new(params[:user])
   if user.valid?
     user.save
@@ -36,7 +46,11 @@ post '/users' do
     redirect '/'
   else
     session[:errors] = user.errors
-    @form_data = params[:user]
+    # p params[:user].methods.sort - Object.methods
+    # p params[:user].to_hash
+    # session[:form_data] = params[:user].to_hash
+    session[:form_data] = convert_for_session(params[:user])
+    # session[:form_data]
     redirect '/users/new'
   end
 end
--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,7 +1,19 @@
 helpers do
 
   def current_user
-    @user = User.find(session[:user_id]) if session[:user_id]
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def errors
+    session.delete(:errors) if session[:errors]
+  end
+
+  def convert_for_session(form_data)
+    data = {}
+    form_data.each do |key, value|
+      data[key.to_sym] = value
+    end
+    data
   end
 
 end
