3699aa5b6e74e49e4aaebe9aa4089675c259dfe0
mattbarackman <matthew.barackman@gmail.com>
2013-06-13 19:00:08 -0700

    Got user stories of signup and signing out working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,20 +12,31 @@ get '/sessions/new' do
 end
 
 post '/sessions' do
-  # sign-in
+  
 end
 
 delete '/sessions/:id' do
-  # sign-out -- invoked via AJAX
+  session[:user_id] = nil
 end
 
 #----------- USERS -----------
 
 get '/users/new' do
   # render sign-up page
+  @errors = session.delete(:errors) if session[:errors] 
   erb :sign_up
 end
 
 post '/users' do
-  # sign-up a new user
+  p params[:user]
+  user = User.new(params[:user])
+  if user.valid?
+    user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    session[:errors] = user.errors
+    @form_data = params[:user]
+    redirect '/users/new'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  #TODO : Use bcrypt to store hashed passwords and authenticate users
+
+  has_secure_password
+
+  validates :name, :presence => true
+  validates :email, :presence => true,
+                    :uniqueness => true
+  validates :password, :presence => true,
+                       :length => {:minimum => 5, :maximum => 40}
+
 end
